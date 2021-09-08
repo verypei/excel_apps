ce32fafb792299b4ae2385222e79f6290c61a0ba
verypei <verrymariyanto@gmail.com>
2021-09-08 14:08:46 +0700

    done

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-const xlsxFile = require('read-excel-file/node');
-let x = document.getElementById("myFile");
- 
-xlsxFile(x).then((rows) => {
-    console.table(rows);
+// const readXlsxFile = require('read-excel-file');
+let input = document.getElementById('myFile');
+input.addEventListener('change',()=>{
+    readXlsxFile(input.files[0])
+                .then((data)=>{
+                     console.log(data);
+                    let i = 0;
+                    let arr = data.filter((el,idx)=>{
+                        return el[3] !== null;
+                    })
+                    
+                    arr.map((row,index)=>{
+                        if(i < 3 ){
+                            let table = document.getElementById('tbl-data');
+                            generateTableHead(table, row, index);
+                        }
+                        if(i > 2){
+                            let table = document.getElementById('tbl-data');
+                            generateTableRows(table, row);
+                        }
+                    })
+                });
 })
 
-// import xlsxFile from 'read-excel-file'
+function generateTableHead(table, data, index){
+    let tHead = table.createTHead();
+    let row = tHead.insertRow();
+    for (let key of data) {
+        let th = document.createElement('th');
+        th.setAttribute('id',`THead${index}`);
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+}
+
+function generateTableRows(table, data){
+    let newRow = table.insertRow(-1);
+    data.map((row,index)=>{
+        let newCell = newRow.insertCell();
+        let newText = newCell.createTextNode(newText);
+    })
+}
